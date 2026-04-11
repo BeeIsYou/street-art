91a3112dff00af1e0d3678e5bd8aab146570cf74
BeeIsYou <beeisyouandbest@gmail.com>
2026-04-11 10:32:00 +0800

    wip spray can model

--- src/client/java/com/streetart/client/StreetArtDataGenerator.java
+++ src/client/java/com/streetart/client/StreetArtDataGenerator.java
@@ -1,11 +1,34 @@
 package com.streetart.client;
 
+import com.streetart.AllItems;
+import net.fabricmc.fabric.api.client.datagen.v1.provider.FabricModelProvider;
 import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+import net.fabricmc.fabric.api.datagen.v1.FabricPackOutput;
+import net.minecraft.client.data.models.BlockModelGenerators;
+import net.minecraft.client.data.models.ItemModelGenerators;
 
 public class StreetArtDataGenerator implements DataGeneratorEntrypoint {
 	@Override
 	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+		FabricDataGenerator.Pack pack = fabricDataGenerator.createPack();
 
+		pack.addProvider(StreetArtModelProvider::new);
+	}
+
+	private static class StreetArtModelProvider extends FabricModelProvider {
+		public StreetArtModelProvider(FabricPackOutput output) {
+			super(output);
+		}
+
+		@Override
+		public void generateBlockStateModels(BlockModelGenerators blockModelGenerators) {
+
+		}
+
+		@Override
+		public void generateItemModels(ItemModelGenerators itemModelGenerators) {
+			itemModelGenerators.declareCustomModelItem(AllItems.sprayCanItem);
+		}
 	}
 }
--- src/main/java/com/streetart/StreetArt.java
+++ src/main/java/com/streetart/StreetArt.java
@@ -6,7 +6,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class StreetArt implements ModInitializer {
-	public static final String MOD_ID = "street-art";
+	public static final String MOD_ID = "street_art";
 
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 	public static Identifier id(String path) {
