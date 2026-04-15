55bc38901c37a5c11121e882392425be15084b12
cyvack <cyvack@outlook.com>
2026-04-14 18:34:48 -0700

    kill blazes

--- src/main/java/com/streetart/item/PressureWasherItem.java
+++ src/main/java/com/streetart/item/PressureWasherItem.java
@@ -4,9 +4,13 @@ import com.streetart.component.ColorComponent;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.particles.ParticleOptions;
 import net.minecraft.core.particles.ParticleTypes;
+import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
+import net.minecraft.world.damagesource.DamageSource;
+import net.minecraft.world.damagesource.DamageTypes;
 import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.monster.Blaze;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
@@ -87,5 +91,16 @@ public class PressureWasherItem extends Item implements SprayPaintInteractor {
     @Override
     public ParticleOptions getParticleAtPoint(final Player player, final ItemStack itemStack) {
         return ParticleTypes.FALLING_WATER;
+    }
+
+    @Override
+    public InteractionResult interactLivingEntity(final ItemStack itemStack, final Player player, final LivingEntity target, final InteractionHand type) {
+        if (!player.level().isClientSide()) {
+            if (target instanceof Blaze) {
+                target.hurtServer((ServerLevel) player.level(), target.damageSources().drown(), 2);
+            }
+        }
+
+        return super.interactLivingEntity(itemStack, player, target, type);
     }
 }
