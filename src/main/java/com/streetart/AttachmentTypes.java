ccb88a87df73365ac6960336254105c284549002
cyvack <cyvack@outlook.com>
2026-05-05 19:08:47 -0700

    hopefully better attachment registry

--- src/main/java/com/streetart/AttachmentTypes.java
+++ src/main/java/com/streetart/AttachmentTypes.java
@@ -3,16 +3,23 @@ package com.streetart;
 import com.streetart.managers.GServerChunkManager;
 import net.fabricmc.fabric.api.attachment.v1.AttachmentRegistry;
 import net.fabricmc.fabric.api.attachment.v1.AttachmentType;
+import net.minecraft.resources.Identifier;
+
+import java.util.function.Consumer;
 
 import static com.streetart.StreetArt.id;
+import static com.streetart.StreetArt.recordingManager;
 
 public class AttachmentTypes {
 
-    public static final AttachmentType<GServerChunkManager> CHUNK_MANAGER = AttachmentRegistry.create(id("graffiti_data"), (AttachmentRegistry.Builder<GServerChunkManager> b) ->
-            b.initializer(GServerChunkManager::new)
-                    .persistent(GServerChunkManager.CODEC)
-                    .buildAndRegister(id("graffiti_data")));
+    public static final AttachmentType<GServerChunkManager> CHUNK_MANAGER = register(id("graffiti_data"), builder -> {
+        builder.initializer(GServerChunkManager::new)
+                .persistent(GServerChunkManager.CODEC);
+    });
 
+    private static <T> AttachmentType<T> register(Identifier id, Consumer<AttachmentRegistry.Builder<T>> consumer) {
+        return AttachmentRegistry.create(id, consumer);
+    }
 
     public static void init() {
     }
